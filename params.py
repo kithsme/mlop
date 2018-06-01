4b212cb8bf7610badd0a0b59b858bf2bc1cd2080
taehun <kithsme@gmail.com>
2018-06-01 16:16:16 +0900

    update param

--- params.py
+++ params.py
@@ -9,7 +9,7 @@ class params:
         self.lonTick = ((self.lonMax)-(self.lonMin))/self.step
         self.latTick = ((self.latMax)-(self.latMin))/self.step
 
-        self.training_set_size = 0.7
+        self.training_set_size = 0.8
         self.test_set_size = 1.0 - self.training_set_size
 
         self.mini_batch_size = 75
@@ -17,7 +17,7 @@ class params:
         self.conv1_filter_size = 6
         self.conv1_stride = 3
         self.conv1_output_size = 32
-        self.conv2_filter_size = 3
+        self.conv2_filter_size = 4
         self.conv2_stride = 2
         self.conv2_output_size = 64
         self.conv3_filter_size = 3
